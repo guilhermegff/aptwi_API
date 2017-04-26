504bf762058d8b9c8023f53575cd32a7a0220ee4
guilhermegff <triclorotolueno@hotmail.com>
2017-04-25 23:07:41 -0700

    WhiteNoise 2

--- apimonitor/settings.py
+++ apimonitor/settings.py
@@ -132,5 +132,5 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
